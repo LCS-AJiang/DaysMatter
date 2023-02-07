e9ea99f4ba5386f24a7d3ccc1671d9c8cc2b947e
Audrey Jiang <ajiang@lcs.on.ca>
2023-02-06 19:10:13 -0500

    Initial Commit

---
+++ DaysMatter/DaysMatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysMatterApp.swift
+//  DaysMatter
+//
+//  Created by Audrey Jiang on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct DaysMatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
